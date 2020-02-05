e220b8f196bfcc3cdea525340c402a9c96ba4948
Chris MacDougald <cmacdougald@alamo.edu>
2020-02-04 18:49:57 -0600

    Add files via upload

---
+++ crm-chp2-ex1/crm-chp2-ex1/crm-chp2-ex1.cpp
@@ -0,0 +1,17 @@
+// crm-chp2-ex1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+	// Input - Variables
+
+	// Processing
+	
+	// Output
+    std::cout << "Varaibles!\n"; 
+
+}
+
